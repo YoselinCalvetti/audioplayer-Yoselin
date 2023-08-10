f2b4fbc3600a6695f9f2832f65fecceb73422267
Yoselin Calvetti <138071248+YoselinCalvetti@users.noreply.github.com>
2023-08-10 15:44:05 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+
+
 //create your first component
 const Home = () => {
+
+const [sounds,setSounds]=useState([])
+
+
+	function obtenerInfo() {
+		fetch('https://playground.4geeks.com/apis/fake/sound/songs')
+		.then((response)=>response.json())
+		.then((data)=>setSounds(data))
+		.catch((error)=>console.log(error))
+	}
+
+	console.log(sounds);
+
+	useEffect(function () {
+			obtenerInfo()
+		 },[])
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container bg-secondary">
+			<div className="row">
+			<div className="col-12  mt-3">
+			<ol className="list-group list-group-numbered bg-dark">
+				{sounds.map(function(item){return <li className="list-group-item bg-dark text-light" key={item.id}>{item.name}</li>})}
+			</ol>
+			</div>
+			</div>
+			<nav class="footer bg-secondary">
+  <div class="container-fluid">
+    <a class="navbar-brand d-flex flex-row justify-content-center" href="#">
+	<i class="fa fa-backward p-3" style={{color: "#ffffff"}}></i>
+	<i class="fa fa-play p-3" style={{color: "#ffffff"}}></i>
+	<i class="fa fa-forward p-3" style={{color: "#ffffff"}}></i>
+    </a>
+  </div>
+</nav>
 		</div>
+
 	);
 };
 
